84afa907a884e8c40a34a291fb5a07d044073edc
Jansen Cruz <103548207+jansencruz23@users.noreply.github.com>
2023-08-12 23:52:52 +0800

    Change forms color

--- Forms/Main.Designer.cs
+++ Forms/Main.Designer.cs
@@ -304,6 +304,10 @@ namespace Job_Order_System
             this.guna2CustomGradientPanel7.Controls.Add(this.txtPartsReplaced);
             this.guna2CustomGradientPanel7.Controls.Add(this.txtDiagError);
             this.guna2CustomGradientPanel7.Controls.Add(this.txtProb);
+            this.guna2CustomGradientPanel7.FillColor = System.Drawing.Color.Gray;
+            this.guna2CustomGradientPanel7.FillColor2 = System.Drawing.Color.Gray;
+            this.guna2CustomGradientPanel7.FillColor3 = System.Drawing.Color.Gray;
+            this.guna2CustomGradientPanel7.FillColor4 = System.Drawing.Color.Gray;
             this.guna2CustomGradientPanel7.Location = new System.Drawing.Point(47, 356);
             this.guna2CustomGradientPanel7.Name = "guna2CustomGradientPanel7";
             this.guna2CustomGradientPanel7.ShadowDecoration.BorderRadius = 20;
@@ -318,6 +322,7 @@ namespace Job_Order_System
             // 
             this.label23.AutoSize = true;
             this.label23.BackColor = System.Drawing.Color.Transparent;
+            this.label23.ForeColor = System.Drawing.Color.White;
             this.label23.Location = new System.Drawing.Point(12, 187);
             this.label23.Name = "label23";
             this.label23.Size = new System.Drawing.Size(57, 19);
@@ -353,7 +358,7 @@ namespace Job_Order_System
             this.label19.AutoSize = true;
             this.label19.BackColor = System.Drawing.Color.Transparent;
             this.label19.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label19.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.label19.ForeColor = System.Drawing.Color.White;
             this.label19.Location = new System.Drawing.Point(80, 14);
             this.label19.Name = "label19";
             this.label19.Size = new System.Drawing.Size(271, 28);
@@ -364,6 +369,7 @@ namespace Job_Order_System
             // 
             this.label14.AutoSize = true;
             this.label14.BackColor = System.Drawing.Color.Transparent;
+            this.label14.ForeColor = System.Drawing.Color.White;
             this.label14.Location = new System.Drawing.Point(12, 140);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(91, 19);
@@ -374,6 +380,7 @@ namespace Job_Order_System
             // 
             this.label13.AutoSize = true;
             this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.ForeColor = System.Drawing.Color.White;
             this.label13.Location = new System.Drawing.Point(11, 96);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(96, 19);
@@ -384,6 +391,7 @@ namespace Job_Order_System
             // 
             this.label12.AutoSize = true;
             this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.ForeColor = System.Drawing.Color.White;
             this.label12.Location = new System.Drawing.Point(11, 51);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(54, 19);
@@ -532,6 +540,10 @@ namespace Job_Order_System
             this.guna2CustomGradientPanel6.Controls.Add(this.txtItemBrand);
             this.guna2CustomGradientPanel6.Controls.Add(this.txtItemDesc);
             this.guna2CustomGradientPanel6.Controls.Add(this.txtORNo);
+            this.guna2CustomGradientPanel6.FillColor = System.Drawing.Color.Gray;
+            this.guna2CustomGradientPanel6.FillColor2 = System.Drawing.Color.Gray;
+            this.guna2CustomGradientPanel6.FillColor3 = System.Drawing.Color.Gray;
+            this.guna2CustomGradientPanel6.FillColor4 = System.Drawing.Color.Gray;
             this.guna2CustomGradientPanel6.Location = new System.Drawing.Point(653, 11);
             this.guna2CustomGradientPanel6.Name = "guna2CustomGradientPanel6";
             this.guna2CustomGradientPanel6.ShadowDecoration.BorderRadius = 20;
@@ -575,7 +587,7 @@ namespace Job_Order_System
             this.label18.AutoSize = true;
             this.label18.BackColor = System.Drawing.Color.Transparent;
             this.label18.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label18.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.label18.ForeColor = System.Drawing.Color.White;
             this.label18.Location = new System.Drawing.Point(144, 10);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(124, 28);
@@ -595,6 +607,7 @@ namespace Job_Order_System
             // 
             this.label17.AutoSize = true;
             this.label17.BackColor = System.Drawing.Color.Transparent;
+            this.label17.ForeColor = System.Drawing.Color.White;
             this.label17.Location = new System.Drawing.Point(26, 430);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(36, 19);
@@ -605,6 +618,7 @@ namespace Job_Order_System
             // 
             this.label16.AutoSize = true;
             this.label16.BackColor = System.Drawing.Color.Transparent;
+            this.label16.ForeColor = System.Drawing.Color.White;
             this.label16.Location = new System.Drawing.Point(26, 386);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(107, 19);
@@ -615,6 +629,7 @@ namespace Job_Order_System
             // 
             this.label15.AutoSize = true;
             this.label15.BackColor = System.Drawing.Color.Transparent;
+            this.label15.ForeColor = System.Drawing.Color.White;
             this.label15.Location = new System.Drawing.Point(26, 338);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(72, 19);
@@ -625,6 +640,7 @@ namespace Job_Order_System
             // 
             this.label20.AutoSize = true;
             this.label20.BackColor = System.Drawing.Color.Transparent;
+            this.label20.ForeColor = System.Drawing.Color.White;
             this.label20.Location = new System.Drawing.Point(28, 281);
             this.label20.Name = "label20";
             this.label20.Size = new System.Drawing.Size(43, 19);
@@ -635,6 +651,7 @@ namespace Job_Order_System
             // 
             this.label11.AutoSize = true;
             this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.ForeColor = System.Drawing.Color.White;
             this.label11.Location = new System.Drawing.Point(26, 219);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(67, 19);
@@ -645,6 +662,7 @@ namespace Job_Order_System
             // 
             this.label10.AutoSize = true;
             this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.ForeColor = System.Drawing.Color.White;
             this.label10.Location = new System.Drawing.Point(26, 93);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(47, 19);
@@ -655,6 +673,7 @@ namespace Job_Order_System
             // 
             this.label9.AutoSize = true;
             this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.ForeColor = System.Drawing.Color.White;
             this.label9.Location = new System.Drawing.Point(26, 176);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(69, 19);
@@ -665,6 +684,7 @@ namespace Job_Order_System
             // 
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.ForeColor = System.Drawing.Color.White;
             this.label8.Location = new System.Drawing.Point(26, 134);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(100, 19);
@@ -675,6 +695,7 @@ namespace Job_Order_System
             // 
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.ForeColor = System.Drawing.Color.White;
             this.label7.Location = new System.Drawing.Point(26, 50);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(92, 19);
@@ -883,6 +904,10 @@ namespace Job_Order_System
             this.guna2CustomGradientPanel5.Controls.Add(this.txtCNum);
             this.guna2CustomGradientPanel5.Controls.Add(this.txtCAddress);
             this.guna2CustomGradientPanel5.Controls.Add(this.txtJobNum);
+            this.guna2CustomGradientPanel5.FillColor = System.Drawing.Color.Gray;
+            this.guna2CustomGradientPanel5.FillColor2 = System.Drawing.Color.Gray;
+            this.guna2CustomGradientPanel5.FillColor3 = System.Drawing.Color.Gray;
+            this.guna2CustomGradientPanel5.FillColor4 = System.Drawing.Color.Gray;
             this.guna2CustomGradientPanel5.Location = new System.Drawing.Point(46, 57);
             this.guna2CustomGradientPanel5.Name = "guna2CustomGradientPanel5";
             this.guna2CustomGradientPanel5.ShadowDecoration.BorderRadius = 20;
@@ -897,6 +922,7 @@ namespace Job_Order_System
             // 
             this.label21.AutoSize = true;
             this.label21.BackColor = System.Drawing.Color.Transparent;
+            this.label21.ForeColor = System.Drawing.Color.White;
             this.label21.Location = new System.Drawing.Point(12, 177);
             this.label21.Name = "label21";
             this.label21.Size = new System.Drawing.Size(86, 19);
@@ -932,7 +958,7 @@ namespace Job_Order_System
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(140, 10);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(151, 28);
@@ -967,6 +993,7 @@ namespace Job_Order_System
             // lblID
             // 
             this.lblID.AutoSize = true;
+            this.lblID.ForeColor = System.Drawing.Color.White;
             this.lblID.Location = new System.Drawing.Point(7, 19);
             this.lblID.Name = "lblID";
             this.lblID.Size = new System.Drawing.Size(40, 19);
@@ -1002,6 +1029,7 @@ namespace Job_Order_System
             // 
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.ForeColor = System.Drawing.Color.White;
             this.label6.Location = new System.Drawing.Point(12, 213);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(53, 19);
@@ -1012,6 +1040,7 @@ namespace Job_Order_System
             // 
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.Color.White;
             this.label5.Location = new System.Drawing.Point(12, 134);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(81, 19);
@@ -1022,6 +1051,7 @@ namespace Job_Order_System
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(12, 93);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(107, 19);
@@ -1032,6 +1062,7 @@ namespace Job_Order_System
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(12, 50);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 19);
